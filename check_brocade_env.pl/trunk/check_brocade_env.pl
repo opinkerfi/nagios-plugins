c8cdc9014706e16689a61e6d15e3273adda7c341
Tómas Edwardsson <tommi@opensource.is>
2010-03-16 15:40:55 +0000

    Fixed licensing

--- check_brocade_env.pl/trunk/check_brocade_env.pl
+++ check_brocade_env.pl/trunk/check_brocade_env.pl
@@ -2,12 +2,12 @@
 #
 # Copyright 2010, Tomas Edwardsson 
 #
-# check_brocade.pl is free software: you can redistribute it and/or modify
+# check_brocade_env.pl is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# check_brocade.pl is distributed in the hope that it will be useful,
+# check_brocade_env.pl is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
