1f9e1444cc09ed2cc579219f714ca50d4077c989
monitoreoDotOsiAtUchileDotCl <monitoreo.osi@uchile.cl>
2020-10-22 10:37:42 -0300

    another minor change, libexec path

--- check_snmp/check_snmp_cpfw.pl
+++ check_snmp/check_snmp_cpfw.pl
@@ -21,8 +21,8 @@ use Getopt::Long;
 
 # Nagios specific
 
-#use lib "/usr/local/nagios/libexec";
-use lib "/usr/lib/nagios/plugins";
+use lib "/usr/local/nagios/libexec";
+#use lib "/usr/lib/nagios/plugins"; # use in ubugtu
 use utils qw(%ERRORS $TIMEOUT);
 #my $TIMEOUT = 15;
 #my %ERRORS=('OK'=>0,'WARNING'=>1,'CRITICAL'=>2,'UNKNOWN'=>3,'DEPENDENT'=>4);
